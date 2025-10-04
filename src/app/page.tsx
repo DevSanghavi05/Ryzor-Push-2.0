3e6e15fa6da6caaa86673be5502f69f083e02ad2
Dev Sanghavi <devrsanghavi05@gmail.com>
2025-10-04 21:46:08 +0000

    why is there so much blank space between the title and the chatbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh bg-background">
       <Header />
-      <main className="flex-1 flex flex-col">
-        <div className="flex flex-col items-center p-4 pt-20 min-h-[calc(100vh-4rem)]">
+      <main className="flex-1 flex flex-col pt-16">
+        <div className="flex flex-col items-center p-4 pt-8 md:pt-12">
           <ChatInterface />
         </div>
         <div className="w-full max-w-5xl mx-auto px-4">
