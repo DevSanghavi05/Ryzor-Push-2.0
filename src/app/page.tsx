0e382e2bf959dbddbfffba671e02ea1b89ad32d2
Dev Sanghavi <devrsanghavi05@gmail.com>
2025-10-04 21:45:23 +0000

    nice, except use scroll fading to seperate the title and chat and upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
         <div className="flex flex-col items-center p-4 pt-20 min-h-[calc(100vh-4rem)]">
           <ChatInterface />
         </div>
+        <div className="w-full max-w-5xl mx-auto px-4">
+            <div className="h-px w-full bg-gradient-to-r from-transparent via-border to-transparent" />
+        </div>
         <Features />
       </main>
     </div>
