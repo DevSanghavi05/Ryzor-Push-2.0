109ebc6f32c3ac4e23706d33242b5d9da0b0ecfb
Dev Sanghavi <devrsanghavi05@gmail.com>
2025-10-07 23:16:44 +0000

    for some reason now the top bar droops down below and covers the top par

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -13,7 +13,6 @@ export default function DocumentPage() {
 
   return (
     <div className="flex flex-col min-h-dvh bg-background">
-      <Header />
       <main className="flex-1 flex flex-col p-4 pt-20 md:p-6 md:pt-24 gap-6">
         <div className="container mx-auto">
           <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { ChatInterface } from '@/components/chat/chat-interface';
 
 export default function Home({ onUploadClick }: { onUploadClick?: () => void }) {
   return (
-    <div className="flex flex-col flex-1 bg-transparent">
+    <div className="flex-1">
       <div className="flex flex-col items-center justify-center h-full p-4 pt-12 md:pt-16">
         <ChatInterface onUploadClick={onUploadClick} />
       </div>
